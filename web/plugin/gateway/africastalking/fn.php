7b6521374fcede57bd5e10d3c4d476e3e35c34d1
janikibichi <janikibichi@gmail.com>
2017-06-25 21:37:15 +0300

    cleanup of fn.php

--- web/plugin/gateway/africastalking/fn.php
+++ web/plugin/gateway/africastalking/fn.php
@@ -36,69 +36,59 @@ function africastalking_hook_sendsms($smsc, $sms_sender, $sms_footer, $sms_to, $
 	}
 
 	if ($sms_sender && $sms_to && $sms_msg) {
-        //create the url call
-        $requestUrl = 'https://api.africastalking.com/version1/messaging';
-        $params = array(
-		    'username' => $plugin_config['africastalking']['api_username'],
-		    'to'       => $sms_to,
-		    'message'  => $sms_msg,
-		);
-        if (function_exists('curl_init')) {
-            	$requestBody = http_build_query($params, '', '&');
+				//create the url call
+				$requestUrl = 'https://api.africastalking.com/version1/messaging';
+				$params = array(
+					'username' => $plugin_config['africastalking']['api_username'],
+					'to'       => $sms_to,
+					'message'  => $sms_msg,
+				);
+				$requestBody = http_build_query($params, '', '&');
 
-                $curlHandle_ = curl_init();
-                curl_setopt($curlHandle_, CURLOPT_POSTFIELDS, $requestBody);
-                curl_setopt($curlHandle_, CURLOPT_POST, 1);
-                curl_setopt($curlHandle_, CURLOPT_HTTPHEADER, array ('Accept: application/json', 'apikey: ' . $plugin_config['africastalking']['api_password']));
+				$curlHandle_ = curl_init($requestUrl);
+				curl_setopt($curlHandle_, CURLOPT_POSTFIELDS, $requestBody);
+				curl_setopt($curlHandle_, CURLOPT_POST, 1);
+				curl_setopt($curlHandle_, CURLOPT_HTTPHEADER, array ('Accept: application/json', 'apikey:'.$plugin_config['africastalking']['api_password']));
 
-                curl_setopt($curlHandle_, CURLOPT_TIMEOUT, 60);
-                curl_setopt($curlHandle_, CURLOPT_SSL_VERIFYPEER, FALSE);
-                curl_setopt($curlHandle_, CURLOPT_URL, $requestUrl);
-                curl_setopt($curlHandle_, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($curlHandle_, CURLOPT_TIMEOUT, 60);
+				curl_setopt($curlHandle_, CURLOPT_SSL_VERIFYPEER, FALSE);
+				curl_setopt($curlHandle_, CURLOPT_RETURNTRANSFER, true);
 
 				$responseBody = curl_exec($curlHandle_);
-						
+					
 				_log("response body from curl".$responseBody, 3, "africastalking_hook_sendsms");
-						
+					
 				$responseInfo = curl_getinfo($curlHandle_);
-						
+					
 				_log("responseInfo from curl".$responseInfo, 3, "africastalking_hook_sendsms");
 				curl_close($curlHandle_);
 
-				_log("sendsms url:[" . $requestUrl . "] callback:[" . $plugin_config['africastalking']['callback_url'], "] smsc:[" . $smsc . "]", 3, "africastalking_hook_sendsms");
-				_log("sendsms response:[" . $reponseBody. "] callback:[" . $plugin_config['africastalking']['callback_url'], "] smsc:[" . $smsc . "]", 3, "africastalking_hook_sendsms");
 				$resp = json_decode($responseBody); 
-
-				if ($resp->status) {
-					if($resp->http_code==201){
-						$c_status = $resp->status;
-						$c_message_id = $resp->messageId;					
-					} else{
-						$c_error_text =	$resp->http_code;				
+				if(count($resp->SMSMessageData->Recipients) > 0){
+					$reslts =$resp->SMSMessageData->Recipients;
+					foreach($reslts as $result) {
+						if ($result->status == 'Sent') {
+							_log("sent smslog_id:" . $smslog_id . " message_id:" .$result->messageId . " status:" . $result->status . " error:" . $c_error_text . " smsc:[" . $smsc . "]", 3, "africastalking_hook_sendsms");
+							$db_query = "
+								INSERT INTO " . _DB_PREF_ . "_gatewayAfricastalking (local_smslog_id,remote_smslog_id,status,error_text)
+								VALUES ('$smslog_id','$result->messageId','$result->status','$c_error_text')";
+							$id = @dba_insert_id($db_query);
+							if ($id && ($c_status == 'Sent')) {
+								$ok = true;
+								$p_status = 0;
+							} else {
+								$p_status = 2;
+							}
+							dlr($smslog_id, $uid, $p_status);	
+						} else {
+							// even when the response is not what we expected we still print it out for debug purposes
+							$resp = str_replace("\n", " ", $resp);
+							$resp = str_replace("\r", " ", $resp);
+							_log("failed smslog_id:" . $smslog_id . " resp:" . $resp . " smsc:[" . $smsc . "]", 3, "africastalking_hook_sendsms");			
+						}  						
 					}
-					_log("sent smslog_id:" . $smslog_id . " message_id:" . $c_message_id . " status:" . $c_status . " error:" . $c_error_text . " smsc:[" . $smsc . "]", 2, "africastalking_hook_sendsms");
-					$db_query = "
-						INSERT INTO " . _DB_PREF_ . "_gatewayAfricastalking (local_smslog_id,remote_smslog_id,status,error_text)
-						VALUES ('$smslog_id','$c_message_id','$c_status','$c_error_text')";
-					$id = @dba_insert_id($db_query);
-					if ($id && ($c_status == 'Sent')) {
-						$ok = true;
-						$p_status = 0;
-					} else {
-						$p_status = 2;
-					}
-					dlr($smslog_id, $uid, $p_status);	
-				} else {
-					// even when the response is not what we expected we still print it out for debug purposes
-					$resp = str_replace("\n", " ", $resp);
-					$resp = str_replace("\r", " ", $resp);
-					_log("failed smslog_id:" . $smslog_id . " resp:" . $resp . " smsc:[" . $smsc . "]", 3, "africastalking_hook_sendsms");			
-				}  				
-
-        } else {
-         _log("fail to sendsms due to missing PHP curl functions", 3, "africastalking_hook_sendsms");
-        }
-     
+				}
+	      
     }
 
 	if (!$ok) {
@@ -106,7 +96,7 @@ function africastalking_hook_sendsms($smsc, $sms_sender, $sms_footer, $sms_to, $
 		dlr($smslog_id, $uid, $p_status);
 	}
 	
-	_log("sendsms end", 3, "twilio_hook_sendsms");
+	_log("sendsms end", 3, "africastalking_hook_sendsms");
 	
 	return $ok;
 }        
